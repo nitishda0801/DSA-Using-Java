7d26d0a48a11c349194be2b2eb91471336cd6e52
nitishda4433 <nitishkr08011997@gmail.com>
2026-02-06 00:06:14 +0530

    commit "0-1 knapsack"

---
+++ src/dp/Knapsack.java
@@ -0,0 +1,10 @@
+package src.dp;
+
+public class Knapsack { // 0-1 Knapsack Problem
+    public static void main(String[] args) {
+        int n = 5;
+        int profit [] = {};
+        int weightv[] = {};
+
+    }
+}
